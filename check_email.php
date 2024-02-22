c298e358f70c011f7b1605b12eee6f6d96899676
IggorGor <72501462+IggorGor@users.noreply.github.com>
2024-02-22 09:56:15 +0300

    Enhance email verification and improve user deletion process
    The commit enhances the process of verifying user email domains utilizing the checkdnsrr function. The code now verifies the domain's MX record instead of only the format. If the domain is invalid and the user has no posts, the code deletes the user and their emails. If not, it only flags the domain as invalid. The mysqli extension is also added to handle deletion operations in a more secure and efficient way.

--- check_email.php
+++ check_email.php
@@ -5,9 +5,12 @@ require_once 'Settings.php';
 
 $connect = mysqli_connect($db_server, $db_user, $db_passwd, $db_name, $db_port);
 
-$query = "select m.id_member, m.email_address, m.posts from members m limit 100";
+$query = "select m.id_member, m.email_address, m.posts from members m";
 $result = mysqli_query($connect, $query);
 
+// Prepare the queries
+$deleteMemberQuery = mysqli_prepare($connect, "DELETE FROM members WHERE id_member=?");
+$deleteMailQueueQuery = mysqli_prepare($connect, "DELETE FROM mail_queue WHERE recipient=?");
 
 ?>
 <!doctype html>
@@ -17,33 +20,38 @@ $result = mysqli_query($connect, $query);
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<title>Удаление зловредных посетителей</title>
 </head>
 <body>
-<table>
+<table class="table table_stripped">
 	<tr>
 		<th>Member ID</th>
 		<th>Email Address</th>
 		<th>Posts</th>
+		<th>Result</th>
 	</tr>
 	<?php
 	while ($row = mysqli_fetch_assoc($result)) {
 		$emailParts = explode('@', $row['email_address']);
 		$domain = array_pop($emailParts);
 
+		$checkDomain = checkdnsrr($domain, 'MX');
 		// If the domain is not valid and there are no posts by the user...
-		if (!checkdnsrr($domain, 'MX') && $row['posts'] === 0) {
-			$deleteMemberQuery = "DELETE FROM members WHERE id_member=" . $row['id_member'];
-			mysqli_query($connect, $deleteMemberQuery);
-			$deleteMailQueueQuery = "DELETE FROM mail_queue WHERE recipient='" . $row['email_address'] . "'";
-			mysqli_query($connect, $deleteMailQueueQuery);
+		if (!$checkDomain)
+			if ($row['posts'] == 0) {
+			mysqli_stmt_bind_param($deleteMemberQuery, 'i', $row['id_member']);
+			mysqli_stmt_execute($deleteMemberQuery);
+			mysqli_stmt_bind_param($deleteMailQueueQuery, 's', $row['email_address']);
+			mysqli_stmt_execute($deleteMailQueueQuery);
 			echoRow($row, "Invalid Domain - User and Associated Mails Deleted");
-		} else if (!checkdnsrr($domain, 'MX')) {
+		} else {
 			echoRow($row, "Invalid Domain - User Not Deleted");
 		}
 	}
 
-	function echoRow($row, $note) {
+	function echoRow($row, $note): void
+	{
 		echo "<tr>";
 		echo "<td>" . $row["id_member"] . "</td>";
 		echo "<td>" . $row["email_address"] . "</td>";
@@ -51,6 +59,8 @@ $result = mysqli_query($connect, $query);
 		echo "<td>" . $note . "</td>";
 		echo "</tr>";
 	}
+
 	?></table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
